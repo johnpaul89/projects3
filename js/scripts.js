ef1d44c6ee8ad8e49e6e3c2c0f67314989df1056
JoPa <johnpaulgitata@gmail.com>
2018-05-04 14:26:47 +0300

    finished user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 //business logic
-var pingPong = function(number){
-  if (number / 3 ){
-  return "Ping"
+var myPingPong = function(number){
+  var output = []
+  if (number % 15 ===0) {
+  $("#numbers").append("<li>pingpong</li>");
 }
-  if (number / 5) {
-    return "Pong"
-  }
-  if (number / 15){
-    return "PingPong"
-  }
+else if (number % 5 === 0){
+  $("#numbers").append("<li>pong</li>");
+}else if (number % 3 === 0) {
+  $("#numbers").append("<li>ping</li>");
+}
+else {
+   $("#numbers").append(number);
+}
 };
 //User Interface
+$(document).ready(function(){
+  $("form#pipo").submit(function(event){
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var result = myPingPong(number);
+
+
+    $("#result").show();
+  });
+});
