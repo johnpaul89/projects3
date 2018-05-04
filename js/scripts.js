a8b5316a727b8e4efc9185ba66c031a16993bfe4
JoPa <johnpaulgitata@gmail.com>
2018-05-04 10:21:57 +0300

    Added Business Logic

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//business logic
+var pingPong = function(number){
+  if (number / 3 ){
+  return "Ping"
+}
+  if (number / 5) {
+    return "Pong"
+  }
+  if (number / 15){
+    return "PingPong"
+  }
+};
+//User Interface
